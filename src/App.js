7028c19ed4e278dca18992276aab6086ef2aa89d
Najim Uddin <najim7304@gmail.com>
2023-03-13 13:51:32 +0600

    deleted all unused

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState([]);
+  const [visible, setVisible] = useState(3);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setItem(data));
+  }, []);
+  const showMoreItems = () => {
+    setVisible((prevValue) => prevValue + 30);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 style={{ textAlign: "center", fontSize: "30px" }}>
+        Load more pagination react
+      </h3>
+      <div className="conttainer">
+        {item.slice(0, visible).map((page) => (
+          <div className="card">
+            <div className="id">
+              <span>{page.id}</span>
+            </div>
+            <p>{page.body}</p>
+          </div>
+        ))}
+        {item.id === 100 ? (
+          <button>Nothing to load</button>
+        ) : (
+          <button onClick={showMoreItems}>Load more</button>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
